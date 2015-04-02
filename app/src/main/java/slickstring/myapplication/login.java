664faae6aa8f4c4c5ef2de3f25d6083e1db950ea
heroisthero <firestinger_1@hotmail.com>
2015-04-02 01:16:09 -0600

    Login disable enabled
    login button will be disabled on empty username field or invalid
    password

--- app/src/main/java/slickstring/myapplication/login.java
+++ app/src/main/java/slickstring/myapplication/login.java
@@ -3,9 +3,11 @@ package slickstring.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,15 +23,49 @@ public class login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupNameListener();
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
+
+    //creates a TextListener to enable the login button when a name is written added
+    private void setupNameListener() {
+        final EditText email = (EditText) findViewById(R.id.emailInput);
+        EditText password = (EditText) findViewById(R.id.passwordInput);
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ((TextView) findViewById(R.id.warningView)).setVisibility(View.INVISIBLE);              //make the invalid password warning disappear
+                if (email.getText().toString().equals("")){
+                    findViewById(R.id.loginButton).setEnabled(false);
+                }
+                else{
+                    findViewById(R.id.loginButton).setEnabled(true);
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ((TextView) findViewById(R.id.warningView)).setVisibility(View.INVISIBLE);              //make the invalid password warning disappear
+                if (!email.getText().toString().equals("")){
+                    findViewById(R.id.loginButton).setEnabled(true);
+                }
+            }
+        });
+
     }
-*/
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -38,6 +74,15 @@ public class login extends Activity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void enableLogin() {
+        if (!((EditText) findViewById(R.id.emailInput)).getText().toString().equals("") && !((EditText) findViewById(R.id.passwordInput)).getText().toString().equals("")){
+            findViewById(R.id.loginButton).setEnabled(true);
+        }
+        else {
+            findViewById(R.id.loginButton).setEnabled(false);
+        }
     }
 
     public void verifyLogin(View view) {
@@ -55,6 +100,7 @@ public class login extends Activity {
             startActivity(intent);
         } else {
             ((TextView) findViewById(R.id.warningView)).setVisibility(View.VISIBLE);                //shows the invalid password warning
+            findViewById(R.id.loginButton).setEnabled(false);
         }
     }
 }
